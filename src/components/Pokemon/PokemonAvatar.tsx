3b3f813f442161ee72d0406fdee3f10477de8b5c
tereytere <teresa.ambroa@hotmail.com>
2024-02-28 14:14:46 +0100

    added pokedex

---
+++ src/components/Pokemon/PokemonAvatar.tsx
@@ -0,0 +1,38 @@
+import { Card, CardContent, CardMedia, Typography } from "@mui/material"
+import { Box } from "@mui/system"
+import { DetailPokemon } from "../../interfaces/pokemon.interface"
+
+interface PokemonAvatarProps {
+  pokemon: DetailPokemon
+}
+
+const PokemonAvatar = ({pokemon}: PokemonAvatarProps) => {
+
+  return (
+    <Card sx={{backgroundColor: pokemon.color}}>
+      <CardMedia 
+          component="img"
+          sx={{height: 100, objectFit: "contain"}}
+          image={pokemon.sprites.other["official-artwork"].front_default}
+          title={pokemon.name}
+        />
+      <CardContent>
+        <Box sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}>
+          <Typography sx={{textTransform: "capitalize"}}>
+            {pokemon.name}
+          </Typography>
+          <Typography sx={{textTransform: "capitalize"}}>
+            #{pokemon.id}
+          </Typography>
+        </Box>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default PokemonAvatar
---
+++ src/components/Pokemon/PokemonList.tsx
@@ -0,0 +1,25 @@
+import { Grid } from "@mui/material";
+import { ListPokemon } from "../../interfaces/pokemon.interface";
+import PokemonCard from "./PokemonCard";
+
+interface PokemonListProps {
+  pokemons: ListPokemon[];
+}
+
+const PokemonList = ({ pokemons }: PokemonListProps) => {
+  return (
+    <Grid container spacing={2}>
+      {pokemons.length > 0
+        ? pokemons.map((p) => {
+            return (
+              <Grid item xs={4}>
+                <PokemonCard key={p.name} pokemon={p} />
+              </Grid>
+            );
+          })
+        : null}
+    </Grid>
+  );
+};
+
+export default PokemonList;
