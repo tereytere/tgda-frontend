6415797b3f82e8dbc0e8d85d7bea4ec8bdd2687c
tereytere <teresa.ambroa@hotmail.com>
2024-06-21 18:49:29 +0200

    readme and welcome text for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,6 @@ import {
 } from "../styledComponents/ContentStyles";
 
 const Home: React.FC = () => {
-
 	return (
 		<BodyContentContainer>
 			<TitleContainer>
@@ -18,13 +17,34 @@ const Home: React.FC = () => {
 				</Title>
 			</TitleContainer>
 			<p>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum
-				adipisci eveniet cum! Aperiam nobis commodi voluptas odit illum unde
-				iure dolores quam. Vel beatae facilis error labore aliquid ab itaque!
+				¿Te preocupa el estado de nuestro planeta?
 				<br />
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum
-				adipisci eveniet cum! Aperiam nobis commodi voluptas odit illum unde
-				iure dolores quam. Vel beatae facilis error labore aliquid ab itaque!
+				¿Quieres contribuir al cuidado del medioambiente pero no sabes por dónde
+				empezar?
+				<br />
+				¡Estás en el lugar correcto!
+				<br />
+				En Tu Granito de Arena, te ofrecemos recursos e información que te
+				ayudarán a adoptar un estilo de vida más sostenible y consciente. Ya sea
+				que quieras cambiar tu dieta, tus hábitos de compra o simplemente
+				aprender más sobre cómo proteger nuestro planeta, tenemos algo para ti.
+				<br />
+				Descubre películas y documentales que inspiran.
+				<br />
+				Aprende con videos educativos y publicaciones de expertos en
+				sostenibilidad y otros temas muy interesantes.
+				<br />
+				Descubre restaurantes, tiendas y protectoras de animales cerca de tí a
+				través de nuestro mapa interactivo.
+				<br />
+				Inspírate con libros y artículos que profundizan en temas
+				medioambientales.
+				<br />
+				Actúa apoyando a organizaciones que limpian nuestro planeta y protegen a
+				sus habitantes. Cada pequeño cambio cuenta.
+				<br />
+				Podemos crear un futuro más verde y saludable para todos los habitantes
+				de este planeta.
 			</p>
 			<Linked>
 				<h2 className="linked">
